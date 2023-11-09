428a717dfd06e5a0ec5be1d419e3226bf1d4ae89
Guilherme Vareiro de Oliveira <90655763+GVO72@users.noreply.github.com>
2023-11-09 22:32:49 +0000

    Added NTP client and new string format
    New String format of pub message that includes correct time synced with NTP server

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,12 @@
 #include "../include/MS01.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <NTPClient.h>
 
-#define SENSOR_PIN 25 //Digital pin of MS01 sensor
+#define SENSOR_PIN 25 // Digital pin of MS01 sensor
 
 /*   Sidenotes
-  Isto está meio instável, deve ser reescrito de forma a usar direitinho o 
-  FreeRTOS mas dada a natureza do protocolo de comunicação do MS01 à mínima diferença
-  de timmings a leitura dá em erro, de qualquer forma assim está "usável".
-  Por vezes as primeiras tentativas após boot dão erro mas eventualmente (após +/- 10 ciclos)
-  começam a sair medições corretas.
-  Outro pormenor está na accuracy das medições, a Sonoff não tem documentação nenhuma
+  Pequeno pormenor na accuracy das medições, a Sonoff não tem documentação nenhuma
   do seu protocolo e apenas com a ajuda do projeto Tasmota (https://tasmota.github.io/docs/)
   é que foi possível ler do sensor e ter uma "escala" mais ou menos usável. De qualquer forma
   os valores ao usar o Tasmota eram de 0.01% em ambiente seco e nunca passavam dos 50% com o
@@ -20,27 +16,37 @@
   Por último, por vezes há umas medidas que saem muito ao lado das suas vizinhas sem que se
   tenha mexido no sensor, por isso o valor final de humidade relativa é uma média entre as
   últimas 3 medições, para desligar basta alterar o "smoothing" do ConvertHumidity de true para
-  false. 
+  false.
   (NÃO ALTERAR TIMMINGS)
 */
 
-const char *ssid = "";                  //SSID
-const char *password = "";              //PASSWORD
+const char *ssid = "";     // SSID
+const char *password = ""; // PASSWORD
 const char *broker = "broker.hivemq.com";
 const int tcpPort = 1883;
 const char *mqttid = "MS01-ESP32/hum";
-char msg[50];
+String msg;
 float hum = 0;
+int curr_time = 0;
+int prev_update_ntp_time = 0;
+int prev_update_epoch_time = 0;
+
+const char *ntp_Server = "pool.ntp.org";
+const int32_t gmt_offset_sec = 0;      // adjust to Portugal timezone
+const int32_t daylight_offset_sec = 0; // adjust this if your location observes DST
+unsigned long epoch_time = 0;
+WiFiUDP wifi_udp_client;
+NTPClient ntp_client(wifi_udp_client, ntp_Server, gmt_offset_sec, daylight_offset_sec);
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-uint8_t sensorPin = 25;  //Pin where sensor is connected
-
 TaskHandle_t task_loop1;
 
-void reconnect(){
-  while(!client.connected()) {
+void reconnect()
+{
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
 
     // Create a random client ID
@@ -48,26 +54,29 @@ void reconnect(){
     clientId += String(random(0xffff), HEX);
     Serial.println("Connecting to MQTT broker");
 
-    if(client.connect(clientId.c_str())) {
+    if (client.connect(clientId.c_str()))
+    {
       Serial.println("Connected");
       client.publish(mqttid, "MQTT Server is Connected");
-
-    } else{
+    }
+    else
+    {
       Serial.println("Trying again in 1 second");
       delay(1000);
-
     }
   }
 }
 
-void setupWifi(){
-  //Setup Wifi
+void setupWifi()
+{
+  // Setup Wifi
   Serial.print("Connecting to ");
   Serial.print(ssid);
   Serial.print(" .");
   WiFi.begin(ssid, password);
 
-  while(WiFi.status() != WL_CONNECTED){
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print(".");
     delay(300);
   }
@@ -77,30 +86,76 @@ void setupWifi(){
   client.setServer(broker, tcpPort);
 }
 
-void another_setup1() {
-  setupWifi();
+void epoch_time_update()
+{
+  int passed_time = (curr_time - prev_update_epoch_time) / 1000;
+  if (passed_time > 0)
+  {
+    epoch_time += passed_time;
+    prev_update_epoch_time = curr_time;
+  }
+
+  if (ntp_client.update())
+  {
+    epoch_time = ntp_client.getEpochTime();
+    // Serial.println("Synced with NTP server.");
+  }
+  prev_update_ntp_time = curr_time;
 }
 
-void another_loop1() {
-  if(!client.connected()){
+String convert_epoch_time_to_string()
+{
+
+  time_t raw_time = static_cast<time_t>(epoch_time);
+  struct tm *time_info;
+  time_info = localtime(&raw_time);
+
+  char date_time_string[50]; // Adjust the size as needed
+  strftime(date_time_string, sizeof(date_time_string), "%Y-%m-%d %H:%M:%S", time_info);
+
+  return String(date_time_string);
+}
+
+void another_setup1()
+{
+  setupWifi();
+
+  while (!ntp_client.update())
+  {
+    // force ntp time update
+  }
+  epoch_time = ntp_client.getEpochTime();
+}
+
+void another_loop1()
+{
+  curr_time = millis();
+  epoch_time_update();
+
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
 
-  snprintf(msg, 50, "RH is: %.2f", hum);
-  client.publish(mqttid, msg);
+  // snprintf(msg, 100, "RH is: %.2f", hum);
+  msg = convert_epoch_time_to_string() + " " + String(hum);
+  client.publish(mqttid, msg.c_str());
 
   delay(2500);
 }
 
-void esploop1(void *param) {
+void esploop1(void *param)
+{
   another_setup1();
-  for (;;){
+  for (;;)
+  {
     another_loop1();
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   MS01.pin = SENSOR_PIN;
   xTaskCreatePinnedToCore(
@@ -111,15 +166,19 @@ void setup() {
       1,
       &task_loop1,
       !ARDUINO_RUNNING_CORE);
-  
 }
 
-void loop() {
-  if(MS01.Read()){
+void loop()
+{
+  if (MS01.Read())
+  {
     hum = MS01.h;
     Serial.println(MS01.h);
-  } else{
+  }
+  else
+  {
     Serial.println("Error");
   }
+
   delay(2000);
 }
